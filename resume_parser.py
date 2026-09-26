5ca2b332f88a6061b7b1bd0ee93ec5b97dc81ba2
Sheikh-Mohammed-Shuhood <shuhood2003@gmail.com>
2026-09-26 23:15:16 +0530

    Change done by s

--- resume_parser.py
+++ resume_parser.py
@@ -20,6 +20,7 @@ def read_pdf(file_path):
                 print(text)
                 print("\n")
 
+
     except FileNotFoundError:
         print(f"Error: The file at '{file_path}' was not found.")
     except Exception as e:
